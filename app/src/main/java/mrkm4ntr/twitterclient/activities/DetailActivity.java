54aebac463829a3d6be84b606b2ee5c1db5a0afd
Shintaro Murakami <mrkm4ntr@gmail.com>
2015-11-04 18:17:21 +0900

    change default floating action button

--- app/src/main/java/mrkm4ntr/twitterclient/activities/DetailActivity.java
+++ app/src/main/java/mrkm4ntr/twitterclient/activities/DetailActivity.java
@@ -1,12 +1,9 @@
 package mrkm4ntr.twitterclient.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
 
 import mrkm4ntr.twitterclient.R;
 import mrkm4ntr.twitterclient.views.DetailFragment;
@@ -20,14 +17,6 @@ public class DetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
 
         if (savedInstanceState == null) {
             Bundle arguments = new Bundle();
