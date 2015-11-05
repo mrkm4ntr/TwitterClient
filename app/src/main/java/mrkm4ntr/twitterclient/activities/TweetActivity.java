1a0ac5f110ec689cd526de2fd9ffd8d42c286365
Shintaro Murakami <mrkm4ntr@gmail.com>
2015-11-05 20:11:44 +0900

    add progress dialog

--- app/src/main/java/mrkm4ntr/twitterclient/activities/TweetActivity.java
+++ app/src/main/java/mrkm4ntr/twitterclient/activities/TweetActivity.java
@@ -2,21 +2,26 @@ package mrkm4ntr.twitterclient.activities;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentValues;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import mrkm4ntr.twitterclient.R;
 import mrkm4ntr.twitterclient.data.TwitterContract;
 import mrkm4ntr.twitterclient.sync.TwitterSyncAdapter;
+import mrkm4ntr.twitterclient.views.ProgressDialogFragment;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.User;
@@ -26,6 +31,10 @@ public class TweetActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = TweetActivity.class.getSimpleName();
 
+    private View mLayout;
+    private Button mTweetButton;
+    private EditText mEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +43,48 @@ public class TweetActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        Button tweetButton = (Button) findViewById(R.id.button_tweet);
-        tweetButton.setOnClickListener(new View.OnClickListener() {
+        mLayout = findViewById(R.id.layout_post_tweet);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        mTweetButton = (Button) findViewById(R.id.button_tweet);
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence text, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() == 0) {
+                    mTweetButton.setEnabled(false);
+                } else {
+                    mTweetButton.setEnabled(true);
+                }
+            }
+        });
+
+        mTweetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String status = editText.getText().toString();
+                String status = mEditText.getText().toString();
                 new TweetTask(status, getApplicationContext()).execute();
             }
         });
+
+        if (mEditText.getText().length() == 0) {
+            mTweetButton.setEnabled(false);
+        }
     }
 
     class TweetTask extends AsyncTask<Void, Void, Status> {
 
         private String mStatus;
         private Context mContext;
+        private DialogFragment mDialogFragment;
 
         public TweetTask(String status, Context context) {
             mStatus = status;
@@ -57,6 +93,7 @@ public class TweetActivity extends AppCompatActivity {
 
         @Override
         protected twitter4j.Status doInBackground(Void... voids) {
+            try {Thread.sleep(3000);}catch(Exception e){}
             try {
                 AccountManager accountManager = AccountManager.get(mContext);
                 Account[] accounts = accountManager.getAccountsByType(
@@ -77,19 +114,41 @@ public class TweetActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            mDialogFragment = ProgressDialogFragment.newInstance(
+                    getString(R.string.message_progress_tweet));
+            mDialogFragment.show(TweetActivity.this.getFragmentManager(), "dialog");
+        }
+
+        @Override
         protected void onPostExecute(twitter4j.Status status) {
             if (status != null) {
                 User user = status.getUser();
                 ContentValues contentValues = new ContentValues();
                 contentValues.put(TwitterContract.StatusEntry._ID, status.getId());
                 contentValues.put(TwitterContract.StatusEntry.COLUMN_TEXT, status.getText());
-                contentValues.put(TwitterContract.StatusEntry.COLUMN_CREATE_AT, status.getCreatedAt().getTime());
+                contentValues.put(TwitterContract.StatusEntry.COLUMN_CREATE_AT,
+                        status.getCreatedAt().getTime());
                 contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_NAME, user.getName());
-                contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_PROFILE_IMAGE_URL, user.getProfileImageURL());
+                contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_PROFILE_IMAGE_URL,
+                        user.getProfileImageURL());
+                contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_SCREEN_NAME,
+                        user.getScreenName());
+                contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_LOCATION,
+                        user.getLocation());
+                contentValues.put(TwitterContract.StatusEntry.COLUMN_USER_BIO,
+                        user.getDescription());
                 getContentResolver().insert(TwitterContract.StatusEntry.CONTENT_URI, contentValues);
                 finish();
             } else {
-                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT);
+                if (mDialogFragment.getShowsDialog()) {
+                    Dialog dialog = mDialogFragment.getDialog();
+                    if (dialog != null) {
+                        dialog.dismiss();
+                    }
+                }
+                Snackbar.make(mLayout, R.string.message_error_post_tweet,
+                        Snackbar.LENGTH_SHORT).show();
             }
         }
     }
